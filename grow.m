672d0c23ccc9d886d70f5a03183c55e2a65c654f
arv-in <73792441+arv-in@users.noreply.github.com>
2021-05-25 16:34:06 +0530

    Add files via upload
    MATLAB function to grow the matrix around initial core structure obtained from gen_core.m function.

---
+++ grow.m
@@ -0,0 +1,56 @@
+function [solid] = grow(s1,N,phi,ps)
+solid = zeros(N,N,N);
+phi_c =1;    % Initially no matrix => poroosity =1
+while (phi_c>phi)
+for i = 1:N
+    for j = 1:N
+        for k = 1:N
+            if s1(i,j,k)==1
+                for x = -1:1
+                    for y = -1:1
+                        for z = -1:1
+                            if (((i + x) > 0 && (j + y) > 0 && (k + z) > 0 && (i + x) < N + 1 && (j + y) < N + 1 && (k + z) < N + 1))
+                                switch (abs(x) + abs(y) + abs(z))
+                                    case 0
+                                        solid(i+x,j+y,k+z) = 1;
+                                    case 1             % face neighbour
+                                        if x~=0
+                                            if (rand() < ps(i,j,k))
+                                                solid(i+x,j+y,k+z) = 1;
+                                            end
+                                        elseif y~=0
+                                            if (rand() < ps(i,j,k))
+                                                solid(i+x,j+y,k+z) = 1;
+                                            end
+                                        elseif z~=0
+                                            if (rand() < ps(i,j,k))
+                                                solid(i+x,j+y,k+z) = 1;
+                                            end
+                                        end
+                                    case 2             % edge neighbour
+                                        pe = ps(i,j,k);
+                                        if (rand() < 0.125*pe)
+                                            solid(i+x,j+y,k+z) = 1;
+                                        end
+                                    case 3             % point neighbour
+                                        pp = ps(i,j,k)/96.0; 
+                                        if (rand() < pp) 
+                                            solid(i+x,j+y,k+z) = 1;
+                                        end
+                                end                               
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+matrix = sum(sum(sum(solid)));
+phi_c = 1-(matrix/(N*N*N))
+
+s1 = solid;
+end
+
+end
